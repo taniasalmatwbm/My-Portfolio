0c02cb9bd88f1237d1f7c8d32f78c63144bb9541
user. name  taniasalmatwbm@gamil.com <taniasalmatwbm@gamil.com>
2025-07-22 14:27:20 +0500

    logoUpdated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,8 @@ function Navbar() {
   return (
     <nav className="mb-20 flex items-center justify-between py-6 ">
       <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-10" src={logo} alt="" />
+        {/* <img className="mx-2 w-10" src={logo} alt="" /> */}
+        <h1> Tania Salamat</h1>
       </div>
 
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
